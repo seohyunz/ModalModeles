257cf74544f6cb726c7aff615204935fae3290d1
seohyunz <joseohyun0201@gmail.com>
2022-06-29 09:38:31 +0900

    #1메세지박스,모달,모달창 MDI작성

--- ModalModeles/Form1.cs
+++ ModalModeles/Form1.cs
@@ -15,6 +15,46 @@ namespace ModalModeles
         public Form1()
         {
             InitializeComponent();
+            IsMdiContainer = true
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요, 아침인사");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요, 아침인사", MessageBoxButtons.YesNoCancel);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요, 아침인사", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm();
+            frm.ShowDialog();
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm();
+            frm.MdiParent = this;
+            frm.Show();
         }
     }
+
+    
+
+
 }
